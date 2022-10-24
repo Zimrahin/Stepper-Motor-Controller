383324e6096512672e1e09652ff7be464c308039
Zimrahin <diegobadillosj@gmail.com>
2022-10-24 15:32:43 -0300

    funciona, pero hay comportamientos extranos para steps pequenos

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 TODO:
 
 - guardar datos en la tarjeta SD sin necesidad de comunicación con el computador
-- cambiar control para que sean angulos absolutos en vez de relativos
-- hacer mediciones promedio para 1 trama: moverse, medir, enviar
-- corregir bug csv, solo guarda primera medición
+
+
+- plot debe ser acorde al angulo absoluto (range(angle: length(data))
+
--- motor_test_serial_test/motor_test_serial_test.ino
+++ motor_test_serial_test/motor_test_serial_test.ino
@@ -5,24 +5,18 @@
 
 // N: step
 // 1.8 deg/N
-
-// Definiciones SD:
-// SDMMCBlockDevice block_device;
-// mbed::FATFileSystem fs("fs");
-
-int lap_delay = 1000; //ms
-// float angle = 720; //deg
-// int step = floor(angle / 1.8);
+// Variables rectas:
+// int Pa = 200;			// Cantidad de pasos para acelerar
+// int Tas = 1600;			// Tiempo inicial entre pasos
+// int Tai = 1000;			// Tiempo final entre pasos (Tmin = 600)
 
 // Definicion de pines:
 #define SETPPIN GPIO_1	//12 (en esquemático)
 #define DIRPIN GPIO_2	//11
 #define ENAPIN GPIO_3	//10
 
-// Variables rectas:
-// int Pa = 200;			// Cantidad de pasos para acelerar
-// int Tas = 1600;			// Tiempo inicial entre pasos
-// int Tai = 1000;			// Tiempo final entre pasos (Tmin = 600)
+
+int currentPos_g = 0; // absolute position "global"
 
 float getSlope(float x1,float y1,float x2,float y2){
 	return (y2-y1)/(x2-x1);
@@ -36,9 +30,9 @@ void forward(int N=400, bool reverse=false, int Pa=200, int Tas=1600, int Tai=10
 	// N numero de pasos
 	// reverse direccion hacia donde gira, reloj o contrareloj
 	unsigned long previousMicros = 0;
-  unsigned long init_time = 0;
-  unsigned long stop_time = 0;
-  unsigned long total_time = 0;
+  	unsigned long init_time = 0;
+  	unsigned long stop_time = 0;
+  	unsigned long total_time = 0;
 	float interval;
 	int n_step = 0;
 	int x = 0;
@@ -74,7 +68,7 @@ void forward(int N=400, bool reverse=false, int Pa=200, int Tas=1600, int Tai=10
 		if (currentMicros - previousMicros >= interval) {
 			previousMicros = currentMicros;
 
-      init_time = micros();
+      		init_time = micros();
 			digitalWrite(SETPPIN,!digitalRead(SETPPIN));
 			
 			if (n_step%2 == 0){
@@ -83,18 +77,66 @@ void forward(int N=400, bool reverse=false, int Pa=200, int Tas=1600, int Tai=10
 				// fprintf(myFile,"%d,%.2f\n",n_step,sensorVoltage);
 				Serial.print(sensorVoltage);
 				Serial.print('-');
-        stop_time = micros();
-        total_time = total_time + (stop_time - init_time);       
+        		stop_time = micros();
+        		total_time = total_time + (stop_time - init_time);       
 			}
 			n_step++;
 		}
 	}  
-	Serial.println();
 	digitalWrite(ENAPIN,HIGH);
-  total_time = total_time/N;
-  Serial.print(total_time);  
+  	total_time = total_time/N;
+  	Serial.print(total_time);  
+	Serial.print('-');
+	// Serial.println(); // \n at the end to let know PC all info has been sent
 }
 
+int calculateStep(String input, int currentPos)
+{
+	int steps_to_move = 0;
+	bool dir_flag = false;
+	int N_lap = 200; //number of steps equal to one lap
+	input.trim(); // remove \n,\r,\0
+	char direction = input[0];
+	String N_rx_string = input.substring(1);
+	int N_rx = N_rx_string.toInt();
+	switch (direction)
+	{
+		case 'l':
+			steps_to_move = N_rx;
+			currentPos = (N_lap + currentPos - N_rx)%N_lap;
+			dir_flag = true;
+			break;
+		case 'r':
+			steps_to_move = N_rx;
+			currentPos = (currentPos + N_rx)%N_lap;
+			dir_flag = false;
+			break;
+		case 'a':
+			steps_to_move = N_rx%N_lap - currentPos;
+			if (abs(steps_to_move) <= N_lap/2) {
+				dir_flag = steps_to_move > 0 ? true : false; //left or right?
+				steps_to_move = abs(steps_to_move);
+			}
+			else {
+				dir_flag = steps_to_move <= 0 ? true : false; //the other way around
+				steps_to_move = N_lap - abs(steps_to_move);
+			}
+			currentPos = N_rx%N_lap;
+			break;
+		default:
+			Serial.println("INVALID");
+			return -1;
+	}
+	forward(steps_to_move, dir_flag);
+	Serial.print(currentPos);
+	Serial.print('-');
+	//debug: motor presents non-consistent behaviour
+	// char debug_char = dir_flag ? 'l' : 'r';
+	// Serial.print(debug_char);
+	// Serial.print(steps_to_move);
+	// Serial.print('-');
+	return currentPos;
+}
 void setup() {
 	Serial.begin(9600);
 	//while (!Serial);
@@ -104,28 +146,16 @@ void setup() {
 
 void loop() 
 {
-	bool dir_flag = false;
+	int resultPos;
 	String receivedString = "0";
 	if (Serial.available() > 0)
 	{
 		receivedString = Serial.readString();
-		receivedString.trim(); // remove \n,\r,\0
-		char direction = receivedString[0];
-		switch (direction)
-		{
-			case 'l':
-				dir_flag = true;
-				break;
-			case 'r':
-				dir_flag = false;
-				break;
-			default:
-				Serial.println("INVALID");
-				return;
+		resultPos = calculateStep(receivedString, currentPos_g);
+		if (resultPos != -1){
+			currentPos_g = resultPos;
 		}
-		String N_rx_string = receivedString.substring(1);
-		int N_rx = N_rx_string.toInt();
-		forward(N_rx,dir_flag);
+		Serial.println(); // \n at the end to let know PC all info has been sent
 	}
 }
 
