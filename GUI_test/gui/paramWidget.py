deeefa6763e05619a67d2eda18cdc2cc6cb49e12
sm.seba <sebastian.sanmartina@sansano.usm.cl>
2022-11-03 13:01:20 -0300

    Param-send

--- GUI_test/gui/paramWidget.py
+++ GUI_test/gui/paramWidget.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QHBoxLayout, QFormLayout, QVBoxLayout, QRadioButton, QSpinBox, QComboBox
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QHBoxLayout, QFormLayout, QVBoxLayout, QRadioButton, QSpinBox, QComboBox, QLabel
 from PyQt5.QtCore import pyqtSignal
 import math
 import numpy as np
@@ -7,19 +7,17 @@ from MessageBox import warningBox
 
 # Constants for operation
 # -> Pa parameters: steps
-LOWEST_PA = 0 
-HIGHEST_PA = 1000
-STEP_INCREMENT = 10
-PA_UNITS = '\t\t\t step'
-PA_TOLERANCE = STEP_INCREMENT/10.0
-
+# LOWEST_PA = 0 
+# HIGHEST_PA = 1000
+# STEP_INCREMENT = 10
+# PA_UNITS = '\t\t\t step'
+# PA_TOLERANCE = STEP_INCREMENT/10.0
 
 # -> Time parameters: microseconds
-MIN_TIME_STEP = 0 # us
-MAX_TIME_STEP = 2000 # us
-TIME_STEP_INCREMENT = 10
-TIME_UNITS = '\t\t\t \u03BCs'
-
+# MIN_TIME_STEP = 0 # us
+# MAX_TIME_STEP = 2000 # us
+# TIME_STEP_INCREMENT = 10
+# TIME_UNITS = '\t\t\t \u03BCs'
 
 class paramWidget(QWidget):
     start_signal = pyqtSignal()
@@ -34,6 +32,10 @@ class paramWidget(QWidget):
         self.step_route = None
         self.status = True
         self.verifed = True
+        # self.Nrev = 200
+        # self.Pa = 0
+        # self.Tai = 0
+        # self.Tas = 0
 
         # Widgets
         # -> Combo boxes
@@ -45,9 +47,12 @@ class paramWidget(QWidget):
         self.Tas_spinbox = QSpinBox()
         self.Tai_spinbox = QSpinBox()
 
+        # -> Output text
+        self.out_lbl = QLabel("")
+
         # -> Radio buttons
-        self.degree_radio = QRadioButton('Degree')
-        self.step_radio = QRadioButton('Step')
+        # self.degree_radio = QRadioButton('Degree')
+        # self.step_radio = QRadioButton('Step')
 
         # -> Bottom buttons
         self.reset_btn = QPushButton('Reset')
@@ -57,19 +62,18 @@ class paramWidget(QWidget):
         self.fields = [self.Pa_spinbox, self.Tas_spinbox, self.Tai_spinbox]
 
         # Init routine 
-        self.SPBoxConfig(self.Pa_spinbox)
         self.apply_btn.setEnabled(True)
-        self.TimeBoxConfig(self.Tai_spinbox)
-        self.TimeBoxConfig(self.Tas_spinbox)
+        # self.SPBoxConfig(self.Pa_spinbox)
+        # self.TimeBoxConfig(self.Tai_spinbox)
+        # self.TimeBoxConfig(self.Tas_spinbox)
 
-        # Signals and slots
-        self.reset_btn.clicked.connect(self.verify)
-        self.apply_btn.clicked.connect(self.StartStopHandler)
+        # Signals and slots (¿es necesario?, ¿como se hace lo de la verificacion?)
+        # self.Pa_spinbox.valueChanged.connect(self.fieldsChanged)
+        # self.Tas_spinbox.valueChanged.connect(self.fieldsChanged)
+        # self.Tai_spinbox.valueChanged.connect(self.fieldsChanged)
 
-        self.Pa_spinbox.valueChanged.connect(self.fieldsChanged)
-        self.Tas_spinbox.valueChanged.connect(self.fieldsChanged)
-        self.Tai_spinbox.valueChanged.connect(self.fieldsChanged)
-
+        # self.reset_btn.clicked.connect(self.verify)
+        self.apply_btn.clicked.connect(self.sendParameters)
 
         # Layout
         v_layout = QVBoxLayout()
@@ -94,137 +98,86 @@ class paramWidget(QWidget):
         btn_row.addWidget(self.apply_btn)
         v_layout.addLayout(btn_row)
 
+        # -> Output text label widget
+        v_layout.addWidget(self.out_lbl)
+
+        # -> Final layout
         self.setLayout(v_layout)
 
         
-    def SPBoxConfig(self, box):
-        box.setMinimum(LOWEST_PA)
-        box.setMaximum(HIGHEST_PA)
-        box.setSingleStep(STEP_INCREMENT)
-        box.setSuffix(PA_UNITS)
+    # def SPBoxConfig(self, box):
+    #     box.setMinimum(LOWEST_PA)
+    #     box.setMaximum(HIGHEST_PA)
+    #     box.setSingleStep(STEP_INCREMENT)
+    #     box.setSuffix(PA_UNITS)
 
-
-    def TimeBoxConfig(self,box):
-        box.setMinimum(MIN_TIME_STEP)
-        box.setMaximum(MAX_TIME_STEP)
-        box.setSingleStep(TIME_STEP_INCREMENT)
-        #box.setDecimals(TIME_DECIMALS)
-        box.setSuffix(TIME_UNITS)
+    # def TimeBoxConfig(self,box):
+    #     box.setMinimum(MIN_TIME_STEP)
+    #     box.setMaximum(MAX_TIME_STEP)
+    #     box.setSingleStep(TIME_STEP_INCREMENT)
+    #     #box.setDecimals(TIME_DECIMALS)
+    #     box.setSuffix(TIME_UNITS)
 
     def getFieldsValues(self):
         ret_dict = {
-            'initial':self.Nrev_spinbox.value(),
-            'final':self.Pa_spinbox.value(),
-            'sp_step':self.Tas_spinbox.value(),
-            'time_step':self.Tai_spinbox.value()
+            'Nrev':self.Nrev_combo.currentText(),
+            'Pa':self.Pa_spinbox.value(),
+            'Tas':self.Tas_spinbox.value(),
+            'Tai':self.Tai_spinbox.value()
         }
         return ret_dict
 
     def colorSpin(self, spin, color='#f86e6c'):
         spin.setStyleSheet('background-color : {};'.format(color))
 
-    def fieldsChanged(self):
-        # If any of the controls fields change and the last values were verified, lock everything as start
-        if(self.verifed):
-            self.verifed = not self.verifed
-            self.apply_btn.setEnabled(False)
+    # def fieldsChanged(self):
+    #     # If any of the controls fields change and the last values were verified, lock everything as start
+    #     if(self.verifed):
+    #         self.verifed = not self.verifed
+    #         self.apply_btn.setEnabled(False)
+    #     else:
+    #         return
 
-        else:
-            return
-
-    def verify(self):
-        values = self.getFieldsValues()
+    # def StartStopHandler(self):
+    #     # Check button status
+    #     btn_status = self.apply_btn.text()
         
-        delta_sp = values['final'] - values['initial']
+    #     if btn_status == 'Start':
+    #         btn_text = 'Stop'
+    #         self.reset_btn.setEnabled(False)
+    #         self.start_signal.emit()
 
-        # Check if Initial and Final SP are over SP_TOLERANCE
-        if (math.isclose(delta_sp, 0.0, rel_tol = PA_TOLERANCE, abs_tol = 0.0)):
-            self.colorSpin(self.Nrev_spinbox)
-            self.colorSpin(self.Pa_spinbox)
-            warning_msg = warningBox('Initial and Final SP rates are too close, less than tolerance: {}'.format(PA_TOLERANCE), self)
-            warning_msg.exec_()
-            self.colorSpin(self.Nrev_spinbox, color='')
-            self.colorSpin(self.Pa_spinbox, color='')
-            return
+    #     else:
+    #         btn_text = 'Start'
+    #         self.reset_btn.setEnabled(True)
+    #         self.stop_signal.emit()
 
-        steps = np.sign(delta_sp)*values['sp_step']
-        f_steps = delta_sp/steps # Needed steps
-        n_steps = math.floor(f_steps) # Complete steps 
-        last_flag = False
+    #     self.apply_btn.setText(btn_text)
 
+    # def stopHandler(self):
+    #     self.reset_btn.setEnabled(True)
+    #     self.apply_btn.setEnabled(True)
 
-        # Check if required steps are factible
-        last_step = 0.0
-        if not (math.isclose(f_steps, n_steps, rel_tol=PA_TOLERANCE, abs_tol=0.0)):
-            last_step = delta_sp - n_steps*steps
-            last_flag = True
+    # def getRoute(self):
+    #     return self.step_route
 
-        # Generate steps for thread
-        step_list = []
-        # 1- Initial point
-        step_list.append(values['initial'])
+    # def lockFields(self):
+    #     self.status = not self.status
 
-        # 2- Generate steps
-        i_step = values['initial']
-        for i in range(n_steps):
-            i_step += steps
-            step_list.append(i_step)
-
-        # 3- Add last step if necessary
-        if(last_flag):
-            i_step += last_step
-            step_list.append(i_step)        
-
-        self.step_route = {
-            'initial':values['initial'],
-            'final':values['final'],
-            'delta_sp':delta_sp,
-            'n_steps':n_steps,
-            'step_size':steps,
-            'last_flag':last_flag,
-            'last_step':last_step,
-            'time_step':values['time_step'],
-            'step_list':step_list
-        }
-
-        # If everything checks, enable start button
-        self.verifed = True
-        self.apply_btn.setEnabled(True)
-
-    def StartStopHandler(self):
-        # Check button status
-        btn_status = self.apply_btn.text()
-        
-        if btn_status == 'Start':
-            btn_text = 'Stop'
-            self.reset_btn.setEnabled(False)
-            self.start_signal.emit()
-
-        else:
-            btn_text = 'Start'
-            self.reset_btn.setEnabled(True)
-            self.stop_signal.emit()
-
-        self.apply_btn.setText(btn_text)
-
-
-    def stopHandler(self):
-        self.reset_btn.setEnabled(True)
-        self.apply_btn.setEnabled(True)
-
-    def getRoute(self):
-        return self.step_route
-
-    def lockFields(self):
-        self.status = not self.status
-
-        for item in self.fields:
-            item.setEnabled(self.status)
+    #     for item in self.fields:
+    #         item.setEnabled(self.status)
 
 
     # AGREGADO 28-10-2022
     def getNrev(self):
-        return self.Nrev_combo.currentText()        
+        return self.Nrev_combo.currentText()   
+            
+    # 03-11-2022:            
+    def sendParameters(self):
+        out_dict = self.getFieldsValues()
+        out_string = 'p-' + str(out_dict['Nrev']) + '-' + str(out_dict['Pa']) + '-' + str(out_dict['Tas']) + '-' + str(out_dict['Tai']) + '\n'
+        self.out_lbl.setText(out_string)
+              
 
 if __name__ == '__main__':
     app = QApplication([])
